f907200b7acfa7b91bcf88510712a676715db1e8
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-08-26 00:47:49 +0900

    add tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+#![feature(test)]
+extern crate test;
+
+use std::collections::BTreeMap;
 use std::env;
 use std::fs::File;
 use std::io;
@@ -7,8 +10,8 @@ use std::io::prelude::*;
 
 struct LTSVParser {}
 impl LTSVParser {
-    fn parse(input_string: String) -> HashMap<String, String> {
-        input_string.split("\t").collect::<Vec<&str>>().iter().fold(HashMap::<String, String>::new(), |mut acc, kv| {
+    fn parse(input_string: String) -> BTreeMap<String, String> {
+        input_string.split("\t").collect::<Vec<&str>>().iter().fold(BTreeMap::<String, String>::new(), |mut acc, kv| {
             let (k, v) = LTSVParser::key_value(kv);
             acc.insert(k.to_string(), v.to_string());
             acc
@@ -32,3 +35,31 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+#[cfg(test)]
+mod tests {
+    use test::Bencher;
+    use LTSVParser;
+    use std::collections::BTreeMap;
+
+    #[test]
+    fn test_key_value() {
+        assert_eq!(("key", "val"), LTSVParser::key_value("key:val"));
+    }
+
+    #[test]
+    fn test_parse() {
+        let line = "A:1\tB:2\tC:3".to_string();
+        let mut r = BTreeMap::<String, String>::new();
+        r.insert("A".to_string(), "1".to_string());
+        r.insert("B".to_string(), "2".to_string());
+        r.insert("C".to_string(), "3".to_string());
+        assert_eq!(r, LTSVParser::parse(line));
+    }
+
+    #[bench]
+    fn bench_parser_btmap(b: &mut Bencher){
+        b.iter(|| {
+            LTSVParser::parse("A:1\tB:2\tC:3".to_string());
+        });
+    }
+}
